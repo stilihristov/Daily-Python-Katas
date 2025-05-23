aa5dd589551b8e6311214d1d7fb208ad229fc787
Stiliyan <stili.hristov@gmail.com>
2025-05-23 09:52:35 +0200

    Add the first steps...

---
+++ Beginnings/fizzbuzz.py
@@ -0,0 +1,12 @@
+# FIZZBUZZ game
+
+import time
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print(f"FizzBuzz + {i}")
+    elif i % 3 == 0:
+        print(f"Fizz + {i}")
+    elif i % 5 == 0:
+        print(f"Buzz + {i}")
+
